cb55e221b253bb86062a3028bf41d06b571c3bad
Spencer Chinn <stchinn@ucsc.edu>
2024-12-13 22:11:58 -0800

    package

--- package.sh
+++ package.sh
@@ -4,5 +4,8 @@ pip3 install unidecode
 echo "Installing BeautifulSoup Requirement"
 pip3 install beautifulsoup4
 
-echo "Installing BeautifulSoup Requirement"
+echo "Installing Sentence Transformers Requirement"
 pip3 install sentence_transformers
+
+echo "Installing Scikit-learn Requirement"
+pip3 install scikit-learn
